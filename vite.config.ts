e89ca370fa4c1ed8c8d16c5fefbdea4f68699f2b
TohidAbdullazade <TohidAbdullazade@gmail.com>
2024-11-20 11:54:31 +0400

    add docker file and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/zehniadmin",
   plugins: [react()],
-})
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3001,
+    origin: "http://0.0.0.0:3001",
+  },
+  preview: {
+    port: 3001,
+    strictPort: true,
+  },
+});
